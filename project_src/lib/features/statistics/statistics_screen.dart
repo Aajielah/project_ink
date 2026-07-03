c1c2194f457191903df784f6b4863ad032932e95
Abdullahi Akanji Tjani <tijaniabdullahi2021@gmail.com>
2026-07-03 17:24:45 +0100

    bugs fixed

--- project_src/lib/features/statistics/statistics_screen.dart
+++ project_src/lib/features/statistics/statistics_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fl_chart/fl_chart.dart';
-import 'package:drift/drift.dart';
+import 'package:drift/drift.dart' hide Column;
 import 'package:intl/intl.dart';
 
 import '../../shared/providers.dart';
