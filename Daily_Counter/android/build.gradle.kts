91c95fea7ff9dbf61a26d9758b48ef3913b15dd3
Abdullahi Akanji Tjani <tijaniabdullahi2021@gmail.com>
2026-09-02 17:26:10 +0100

    fix(android): inject namespace fallback for isar_flutter_libs with AGP 8+

--- Daily_Counter/android/build.gradle.kts
+++ Daily_Counter/android/build.gradle.kts
@@ -15,6 +15,30 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+subprojects {
+    val configureAndroid = {
+        if (project.plugins.hasPlugin("com.android.application") ||
+            project.plugins.hasPlugin("com.android.library")) {
+            val android = project.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+            if (android != null && android.namespace == null) {
+                android.namespace = if (project.group.toString().isNotEmpty()) {
+                    project.group.toString()
+                } else {
+                    "dev.isar.${project.name.replace('-', '_')}"
+                }
+            }
+        }
+    }
+    if (project.state.executed) {
+        configureAndroid()
+    } else {
+        project.afterEvaluate {
+            configureAndroid()
+        }
+    }
+}
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
